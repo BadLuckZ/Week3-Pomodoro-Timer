bb6e498fe816515dae26f74fb01c5bb65315b6ea
BadLuckZ <thanagorn8802@gmail.com>
2024-09-22 14:03:17 +0700

    Control Time by using Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [timeSession, setTimeSession] = useState(25);
+  const [timeBreak, setTimeBreak] = useState(5);
+
   return (
     <>
       <h1 className="text-smoke text-center">Pomodoro Timer</h1>
       <div className="bg-redwood flex flex-col items-center px-[40px]">
         <div className="setting-box">
           <h2>Session</h2>
-          <h3>25:00</h3>
+          <h3>{timeSession}</h3>
           <div className="flex justify-between w-full">
             <button className="btn">Start</button>
             <button className="btn">Reset</button>
@@ -14,18 +19,48 @@ function App() {
         <div className="flex justify-between w-full">
           <div className="setting-box">
             <h2>Session Timer</h2>
-            <h3>25</h3>
+            <h3>{timeSession}</h3>
             <div className="flex justify-between w-full">
-              <button className="rounded-btn">-</button>
-              <button className="rounded-btn">+</button>
+              <button
+                className="rounded-btn"
+                onClick={() => {
+                  setTimeSession((time) => time - 1);
+                }}
+                disabled={timeSession == 0}
+              >
+                -
+              </button>
+              <button
+                className="rounded-btn"
+                onClick={() => {
+                  setTimeSession((time) => time + 1);
+                }}
+              >
+                +
+              </button>
             </div>
           </div>
           <div className="setting-box">
             <h2>Break Timer</h2>
-            <h3>5</h3>
+            <h3>{timeBreak}</h3>
             <div className="flex justify-between w-full">
-              <button className="rounded-btn">-</button>
-              <button className="rounded-btn">+</button>
+              <button
+                className="rounded-btn"
+                onClick={() => {
+                  setTimeBreak((time) => time - 1);
+                }}
+                disabled={timeBreak == 0}
+              >
+                -
+              </button>
+              <button
+                className="rounded-btn"
+                onClick={() => {
+                  setTimeBreak((time) => time + 1);
+                }}
+              >
+                +
+              </button>
             </div>
           </div>
         </div>
